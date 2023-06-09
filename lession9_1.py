cae42990181ff86bdad787d2a0b3c1af5f74b5b3
VansonLi <lisheng105@gmail.com>
2023-06-09 11:24:02 +0800

    submit

---
+++ lession9_1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown("Hello Word!")
+st.divider()
+st.title("這是APP的TITLE")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider()
+st.code("a=123")
