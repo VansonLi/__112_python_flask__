00138bf5939cbdcf80c1874781104347ed80aa62
VansonLi <77630468+VansonLi@users.noreply.github.com>
2023-06-10 08:17:44 +0000

    submit

--- lession12_1.py
+++ lession12_1.py
@@ -13,11 +13,13 @@ def fetch_stock_dataFrame(id):
    fetch_stock_dataFrame = yf.download(id,start='2022-01-01')
    return   fetch_stock_dataFrame
     
+chratDataFrame = None
 for code in selected_codes:
    code1 = code[:4]+".TW"
    code_stock_dataFrame = fetch_stock_dataFrame(code1)
    code_stock_dataFrame_sorted = code_stock_dataFrame.sort_index(ascending=False)
    st.subheader(code)
+   chratDataFrame
    st.dataframe(code_stock_dataFrame_sorted,width=1024)
    st.line_chart(code_stock_dataFrame_sorted,y='Adj Close')
    st.divider()
